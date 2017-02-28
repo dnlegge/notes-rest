f44d2fb16785fc7147528a8d66eaed525e366b63
dnlegge <david@dnlegge.com>
2017-02-28 21:43:08 +0000

    Sparse running application - index.html and rest result OK

--- notes-rest-web/src/main/java/com/dnlegge/notes/web/rest/controller/NotesRestController.java
+++ notes-rest-web/src/main/java/com/dnlegge/notes/web/rest/controller/NotesRestController.java
@@ -5,14 +5,30 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping(value = "rest/",
-        produces = MediaType.APPLICATION_JSON_VALUE)
 public class NotesRestController {
     private static final Logger LOGGER = LoggerFactory.getLogger(NotesRestController.class);
 
+    public String index() {
+        return "index";
+    }
 
+    public String home() {
+        return "home";
+    }
+
+    @RequestMapping(value = "rest",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public String run() {
+
+        return "OK";
+
+    }
 }
